b9271707c37c01fad02f3f2f502b979305735d76
WeberRomain <romain.weber@carbon-it.com>
2025-02-28 14:11:35 +0100

    Initial commit

---
+++ src/test/java/io/weber/agregio/AgregioApplicationTests.java
@@ -0,0 +1,13 @@
+package io.weber.agregio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
