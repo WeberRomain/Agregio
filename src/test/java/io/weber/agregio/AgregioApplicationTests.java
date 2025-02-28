ed62136c6c5a8800fb826a3e77bd9912bf0b0173
WeberRomain <romain.weber@carbon-it.com>
2025-02-28 14:14:56 +0100

    init properties + config

--- src/test/java/io/weber/agregio/AgregioApplicationTests.java
+++ src/test/java/io/weber/agregio/AgregioApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AgregioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
